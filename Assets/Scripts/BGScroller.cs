8e75224884acfb9ec1793dd79ec24ea2510a632f
Pham Quang Thien <thienpq88@gmail.com>
2017-04-08 22:58:46 +0700

    2nd

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour {
+	public float scrollSpeed;
+	public float tileSizeZ;
+
+	private Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newPos = Mathf.Repeat (Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPos + Vector3.forward * newPos;
+		
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-	public GameObject hazard;
+	public GameObject[] hazards;
 	public Vector3 spawnValues;
 	public int hazardCount;
 	public float spawnWait;
@@ -37,9 +37,10 @@ public class GameController : MonoBehaviour {
 		yield return new WaitForSeconds (startWait);
 		while (true) {
 			for (int i = 0; i < hazardCount; i++) {
+				GameObject hazard = hazards[Random.Range(0, hazards.Length)];
 				Vector3 spawnPosition = new Vector3 (
-					                       Random.Range (-spawnValues.x, spawnValues.x),
-					                       spawnValues.y, spawnValues.z);
+                       Random.Range (-spawnValues.x, spawnValues.x),
+                       spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
 				Instantiate (hazard, spawnPosition, spawnRotation);
 				yield return new WaitForSeconds (spawnWait);
